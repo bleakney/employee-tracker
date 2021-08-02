dfea759cae616833c3dbf0c9337341e57e29e988
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-01 19:01:48 -0500

    update seeds, update mysql query for 'view all employees' request

--- server.js
+++ server.js
@@ -1,13 +1,283 @@
-// set up dependencies
-const inquirer = require('inquirer');
-const express = require('express');
-const cTable = require('console.table');
-const db = require('./db/connection')
+// dependencies
+const inquirer = require("inquirer");
+const express = require("express");
+const cTable = require("console.table");
+const db = require("./db/connection");
 
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const promptValidation = (promptAnswer) => {
+  if (promptAnswer) {
+    return true;
+  } else {
+    console.log("Please enter a valid answer!");
+  }
+};
 
 // express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// variable declarations
+const dbOptionsArray = [
+  "View all departments",
+  "View all roles",
+  "View all employees",
+  "Add a department",
+  "Add a role",
+  "Add an employee",
+  "Update an employee role",
+];
+
+const promptUser = async () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "dbOptions",
+        message: "What would you like to do?",
+        choices: dbOptionsArray,
+      },
+    ])
+    .then(({ dbOptions }) => {
+      switch (dbOptions) {
+        case dbOptionsArray[0]:
+          // show departments table
+          showDepartments();
+          break;
+        case dbOptionsArray[1]:
+          // show roles table
+          showRoles();
+          break;
+        case dbOptionsArray[2]:
+          // show employees table
+          showEmployees();
+          break;
+        case dbOptionsArray[3]:
+          // add a department
+          addDepartment();
+          break;
+        case dbOptionsArray[4]:
+          // add a role
+          addRole();
+          break;
+        case dbOptionsArray[5]:
+          // add an employee
+          addEmployee();
+          break;
+        case dbOptionsArray[6]:
+          // update an employee's role
+          updateRole();
+          break;
+      }
+    });
+};
+
+const showDepartments = () => {
+  db.query("SELECT * FROM departments", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    const table = cTable.getTable(result);
+    console.log(table);
+    promptUser();
+  });
+};
+
+const showRoles = () => {
+  db.query("SELECT * FROM roles", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    const table = cTable.getTable(result);
+    console.log(table);
+    promptUser();
+  });
+};
+
+const showEmployees = () => {
+  db.query(
+    `SELECT a.first_name as First_Name, 
+    a.last_name as Last_Name,
+    roles.title AS Job_Title,
+    roles.salary AS Salary,
+    departments.name as Department,
+    CONCAT_WS(' ', b.first_name, b.last_name) AS Manager
+    FROM employees a
+    JOIN roles ON a.role_id = roles.id
+    JOIN departments ON roles.dept_id = departments.id
+    INNER JOIN employees b ON a.manager_id = b.id`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      const table = cTable.getTable(result);
+      console.log(table);
+      promptUser();
+    }
+  );
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message:
+          "Please enter a name for the department you would like to add to the database.",
+        validate: (departmentName) => promptValidation(departmentName),
+      },
+    ])
+    .then(({ departmentName }) => {
+      db.query(
+        "INSERT INTO departments (name) VALUES (?)",
+        departmentName,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(
+              "Successfully added " + departmentName + " to the database!"
+            );
+            showDepartments();
+            promptUser();
+          }
+        }
+      );
+    });
+};
+
+const addRole = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleTitle",
+        message:
+          "Please enter a title for the role you would like to add to the database.",
+        validate: (roleTitle) => promptValidation(roleTitle),
+      },
+      {
+        type: "number",
+        name: "salary",
+        message: "Please enter the salary for this role using xxxxx.xx format.",
+        validate: (salary) => promptValidation(salary),
+      },
+      {
+        type: "number",
+        name: "deptId",
+        message: "Please enter the department ID for this role.",
+        validate: (deptId) => promptValidation(deptId),
+      },
+    ])
+    .then((answers) => {
+      let answersArray = Object.values(answers);
+      db.query(
+        "INSERT INTO roles (title, salary, dept_id) VALUES (?,?,?)",
+        answersArray,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(
+              "Successfully added " + answersArray[0] + " to the database!"
+            );
+            showRoles();
+            promptUser();
+          }
+        }
+      );
+    });
+};
+
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Please enter the employee's first name.",
+        validate: (firstName) => promptValidation(firstName),
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Please enter the employee's last name.",
+        validate: (lastName) => promptValidation(lastName),
+      },
+      {
+        type: "number",
+        name: "roleId",
+        message:
+          "What is this employee's role? Please enter '1' for 'manager', '2' for 'intern', '3' for engineer, '4' for 'beach boy', or '5' for 'spice girl'.",
+        validate: (roleId) => promptValidation(roleId),
+      },
+      {
+        type: "number",
+        name: "managerId",
+        message:
+          "Who is this employee's manager? Enter '1' for Michael Scott, '2' for Jim Halpert, or '3' for Posh Spice. ",
+        validate: (managerId) => promptValidation(managerId),
+      },
+    ])
+    .then((answers) => {
+      let answersArray = Object.values(answers);
+      db.query(
+        "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+        answersArray,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(
+              "Successfully added " +
+                answersArray[0] +
+                " " +
+                answersArray[1] +
+                " to the database!"
+            );
+            showEmployees();
+            promptUser();
+          }
+        }
+      );
+    });
+};
+
+const updateRole = () => {
+  inquirer
+    .prompt([
+      {
+        type: "number",
+        name: "employeeId",
+        message:
+          "Please enter the id number of the employee whose role you would like to update.",
+        validate: (employeeId) => promptValidation(employeeId),
+      },
+      {
+        type: "number",
+        name: "roleId",
+        message: "Please enter the id number of the employee's new role.",
+        validate: (roleId) => promptValidation(roleId),
+      },
+    ])
+    .then((answers) => {
+      let answersArray = Object.values(answers);
+      answersArray = answersArray.reverse();
+      db.query(
+        "UPDATE employees SET role_id = ? WHERE id = ?",
+        answersArray,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully updated the database!");
+            showEmployees();
+            promptUser();
+          }
+        }
+      );
+    });
+};
+
+promptUser();
